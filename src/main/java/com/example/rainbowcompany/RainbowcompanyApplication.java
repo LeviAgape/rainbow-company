2729fbcbf53dfb21760f1197acc3bbdded344ae0
Levi <agapelevi99@gmail.com>
2025-03-12 14:22:26 -0300

    chore/setup

---
+++ src/main/java/com/example/rainbowcompany/RainbowcompanyApplication.java
@@ -0,0 +1,14 @@
+package com.example.rainbowcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class RainbowcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainbowcompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rainbowcompany/RainbowcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rainbowcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainbowcompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
