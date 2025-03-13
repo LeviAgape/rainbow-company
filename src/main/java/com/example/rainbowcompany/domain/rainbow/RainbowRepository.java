c36634974a96bdccad4fd1740556fe64e6bb4d6e
Levi <agapelevi99@gmail.com>
2025-03-12 22:36:52 -0300

    fix: changed names to controllers

--- src/main/java/com/example/rainbowcompany/domain/rainbow/RainbowRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.rainbowcompany.domain.rainbow;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RainbowRepository extends JpaRepository<Rainbow, String> {
-}
