dabd88a969e0cdc38f957da4d7afa11f0eff08cc
Levi <agapelevi99@gmail.com>
2025-03-15 20:32:41 -0300

    feat: Create Cors to connect with frontEnd URL

--- src/main/java/com/example/rainbowcompany/controller/UserController.java
+++ src/main/java/com/example/rainbowcompany/controller/UserController.java
@@ -3,14 +3,19 @@ package com.example.rainbowcompany.controller;
 import com.example.rainbowcompany.domain.user.RequestUser;
 import com.example.rainbowcompany.domain.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "${frontend.url}")
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
 
     @Autowired
     private UserService userService;
