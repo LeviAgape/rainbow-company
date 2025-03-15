1ae7d1b4eb14a351e6af298b88ddf0a4ffb13bfc
Levi <agapelevi99@gmail.com>
2025-03-15 20:34:52 -0300

    fix: excluded comments

--- src/main/java/infra/cors/CorsConfig.java
+++ src/main/java/infra/cors/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(frontendUrl) // Usa a variável configurada
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
